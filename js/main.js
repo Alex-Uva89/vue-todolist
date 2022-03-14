2b6a7716510388859ca357201ebb83033594c767
Alex-Uva89 <a.uva1988@gmail.com>
2022-03-14 16:34:44 +0100

    addtodo

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 const app = new Vue({
     el:'#js-root',
     data:{
+        newTodo: '',
         todos: [
             {
                 name: 'Esempio prova',
                 condition: 'true', 
             },
-            {
-                name: 'Esempio prova',
-                condition: 'true', 
-            },
-            {
-                name: 'Esempio prova',
-                condition: 'true', 
-            },
-            {
-                name: 'Esempio prova',
-                condition: 'true', 
-            },
+            
         ]
+    },
+    methods: {
+        delteTodo(element){
+            this.todos.splice(element, 1);
+        },
+        addTodo(){
+            this.todos.push(newTodo);
+        }
     }
 
     
