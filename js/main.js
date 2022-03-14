608937db3267a2b1cfe7556d1abb8cd4f9e7f7c2
Alex-Uva89 <a.uva1988@gmail.com>
2022-03-14 16:14:49 +0100

    try

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+    el:'#js-root',
+    data:{
+        todos: [
+            {
+                name: 'Esempio prova',
+                condition: 'true', 
+            },
+            {
+                name: 'Esempio prova',
+                condition: 'true', 
+            },
+            {
+                name: 'Esempio prova',
+                condition: 'true', 
+            },
+            {
+                name: 'Esempio prova',
+                condition: 'true', 
+            },
+        ]
+    }
+
+    
+  })
